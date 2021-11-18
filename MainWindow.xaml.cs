b23f8d097d3e495e7fec0168dedf338a6fd907d5
Alexei.andriyanov@gmail.com <Alexei.andriyanov@gmail.com>
2021-11-18 09:27:09 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+//Разработать в WPF приложении класс WeatherControl, моделирующий погодную сводку
+//– температуру (целое число в диапазоне от -50 до +50),
+//направление ветра(строка), скорость ветра(целое число),
+//наличие осадков(возможные значения: 0 – солнечно, 1 – облачно, 2 – дождь, 3 – снег.
+//Можно использовать целочисленное значение, либо создать перечисление enum).
+//Свойство «температура» преобразовать в свойство зависимости.
+
+namespace Lab_2_6
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WeatherControl.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+//Разработать в WPF приложении класс WeatherControl, моделирующий погодную сводку
+//– температуру (целое число в диапазоне от -50 до +50),
+//направление ветра(строка), скорость ветра(целое число),
+//наличие осадков(возможные значения: 0 – солнечно, 1 – облачно, 2 – дождь, 3 – снег.
+//Можно использовать целочисленное значение, либо создать перечисление enum).
+//Свойство «температура» преобразовать в свойство зависимости. 
+
+namespace Lab_2_6
+{
+    class WeatherControl : DependencyObject
+    {
+        public string WindDirection { get; set; }
+
+        public int WindSpeed { get; set; }
+
+        public enum Railfall
+        {
+            Sunny = 0,
+            Cloudy = 1,
+            Rainy = 2,
+            Snowy = 3
+        }
+
+        public int Temperature
+        {
+            get => (int)GetValue(TemperatureProperty);
+            set => SetValue(TemperatureProperty, value);
+        }
+
+        public static readonly DependencyProperty TemperatureProperty;
+
+        static WeatherControl()
+        {
+            TemperatureProperty = DependencyProperty.Register(
+                nameof(Temperature),
+                typeof(int),
+                typeof(WeatherControl),
+                new FrameworkPropertyMetadata(
+                    0,
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.AffectsRender,
+                    null,
+                    new CoerceValueCallback(CoerceTemperature)),
+                new ValidateValueCallback(ValidateTemperature));
+        }
+
+        public static object CoerceTemperature(DependencyObject d, object baseValue)
+        {
+            int v = (int)baseValue;
+            if (v >= -50 && v <= 50)
+            {
+                return v;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public static bool ValidateTemperature(object value)
+        {
+            int v = (int)value;
+            if (v >= -50 && v <= 50)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
